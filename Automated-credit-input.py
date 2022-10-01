7a746f8a5a860b24556f0f8cc4468084b1f5831b
Shogo Hirasawa <origami.no.1@gmail.com>
2022-10-02 00:03:45 +0900

    2022/10/1
    Multiple files can now be selected and saved with "new_*".

--- Automated-credit-input.py
+++ Automated-credit-input.py
@@ -1,13 +1,14 @@
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageFont
 import tkinter
 from tkinter import filedialog
+import os
 
-fle = tkinter.filedialog.askopenfilenames(filetypes=[('', '*')],initialdir = "{PWD}")
+fle = tkinter.filedialog.askopenfilenames(filetypes=[('', '*')],initialdir = "{PWD}") 
 
 for i in fle:
-  opendimage = Image.open(str(i))
-  output_image = ImageDraw.Draw(opendimage)
-  output_image.text((28, 36), "hello world", fill=(255, 0, 0))
-  opendimage.save("test.png")
-
+  input_image = Image.open(str(i))
+  output_image = ImageDraw.Draw(input_image)
+  output_image.text((50, 50),"Satellite Imagery © Maxar Technologies Provided by European Space Imaging", fill=(0, 0, 0),)
+  input_image.save("new_" + os.path.basename(i) )
